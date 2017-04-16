43034c6783624872d478e2477a42b491bdbba8a0
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2017-04-16 03:00:00 +0300

    travis

--- monoinstall.sh
+++ monoinstall.sh
@@ -1,7 +1,7 @@
 PREFIX=/usr/local
 VERSION=4.8.0.524
 wget "https://download.mono-project.com/sources/mono/mono-$VERSION.tar.bz2"
-tar xf mono-$VERSION.tar.bz2
+tar xjf mono-$VERSION.tar.bz2
 cd mono-$VERSION
 ./configure --prefix=$PREFIX --disable-nls
 make
