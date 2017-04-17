15119c3221df58c831666d8ec2acd674f0ae7c0b
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2017-04-17 19:15:20 +0300

    travis

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cp -R $TRAVIS_BUILD_DIR/release-artifacts/libs/libtensorflow.dylib $TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests/bin/Release/
+cp -R $TRAVIS_BUILD_DIR/release-artifacts/libs/libtensorflow.so $TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests/bin/Release/
 cd $TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests/bin/Release
 mono xunit.console.exe TensorFlowSharp.Tests.dll xunit.console.exe.config
