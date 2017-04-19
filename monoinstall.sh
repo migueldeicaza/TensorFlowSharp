30b0916fc162ba055ce084a50804b8957f517e13
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2017-04-19 20:31:21 +0300

    travis switched to migueldeicaza

--- monoinstall.sh
+++
@@ -1,9 +0,0 @@
-PREFIX=/usr/local
-brew install autoconf automake libtool pkg-config cmake
-wget "https://download.mono-project.com/sources/mono/mono-4.8.0.524.tar.bz2"
-tar xjf mono-4.8.0.524.tar.bz2
-cd mono-4.8.0
-./configure --prefix=$PREFIX --disable-nls
-make
-make install
-cd $TRAVIS_BUILD_DIR
