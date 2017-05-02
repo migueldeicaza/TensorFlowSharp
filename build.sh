5d6890cd82e3fd103cb12732a33f7a151397451a
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2017-05-02 10:57:31 +0300

    removed libtensorflow.dll

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests/bin/Release
-mono --arch=64 xunit.console.exe TensorFlowSharp.Tests.dll xunit.console.exe.config
+mono --arch=64 xunit.console.exe TensorFlowSharp.Tests.dll
