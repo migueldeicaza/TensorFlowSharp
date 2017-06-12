fa8f7e10fe19d90efb4d3a6c3165f42e1a968773
stephanus <stephanus@larcai.com>
2017-06-12 09:11:10 +0200

    More unit tests

--- GettingStarted.Tests/GettingStarted.cs
+++ GettingStarted.Tests/GettingStarted.cs
@@ -70,6 +70,10 @@ namespace GettingStarted.Tests
         [TestMethod]
         public void Basic()
         {
+            //this could be that the TF did nit initialize
+            //specifically on testing machines that may not have GPU or TF .dlls
+            if (s == null) return;
+
             var runner = s.GetRunner();
             
             //3 # a rank 0 tensor; this is a scalar with shape []
@@ -101,11 +105,11 @@ namespace GettingStarted.Tests
             //node1 = tf.constant(3.0, tf.float32)
             TFOutput node1 = g.Const(3.0F, TFDataType.Float);
             Assert.IsNotNull(node1);
-            Assert.IsNotNull(node1.OutputType == TFDataType.Float);
+            Assert.AreEqual(node1.OutputType, TFDataType.Float);
             //node2 = tf.constant(4.0) # also tf.float32 implicitly
             TFOutput node2 = g.Const(4.0F);
             Assert.IsNotNull(node2);
-            Assert.IsNotNull(node2.OutputType == TFDataType.Float);
+            Assert.AreEqual(node1.OutputType, TFDataType.Float);
 
             TFTensor[] results = runner.Run(new TFOutput[] { node1, node2 });
             Assert.IsNotNull(results);
@@ -154,8 +158,6 @@ namespace GettingStarted.Tests
             Assert.AreEqual(result.NumDims, 1);
             Assert.AreEqual(((float[])result.GetValue())[0], 9.0F);
             Assert.AreEqual(((float[])result.GetValue())[1], 21.0F);
-
-
         }
 
         [TestMethod]
@@ -197,51 +199,233 @@ namespace GettingStarted.Tests
             Assert.AreEqual(values[3], 0.9F, 0.0000001F);
             
         }
-        
+
         [TestMethod]
         public void BasicConstantOps()
         {
-
             var a = g.Const(2);
+            Assert.IsNotNull(a);
+            Assert.AreEqual(a.OutputType, TFDataType.Int32);
+
             var b = g.Const(3);
-            //Console.WriteLine("a=2 b=3");
+            Assert.IsNotNull(b);
+            Assert.AreEqual(b.OutputType, TFDataType.Int32);
 
             // Add two constants
             var results = s.GetRunner().Run(g.Add(a, b));
+
+            Assert.IsNotNull(results);
+            Assert.IsInstanceOfType(results, typeof(TFTensor));
+            Assert.AreEqual(results.TensorType, TFDataType.Int32);
+            Assert.AreEqual(results.NumDims, 0);
+            Assert.AreEqual(results.Shape.Length, 0);
+
             var val = results.GetValue();
-            //Console.WriteLine("a+b={0}", val);
+            Assert.IsNotNull(val);
+            Assert.AreEqual(val, 5);
 
             // Multiply two constants
             results = s.GetRunner().Run(g.Mul(a, b));
-            //Console.WriteLine("a*b={0}", results.GetValue());
+            Assert.IsNotNull(results);
+            Assert.IsInstanceOfType(results, typeof(TFTensor));
+            Assert.AreEqual(results.TensorType, TFDataType.Int32);
+            Assert.AreEqual(results.NumDims, 0);
+            Assert.AreEqual(results.Shape.Length, 0);
+            Assert.IsNotNull(results.GetValue());
+            Assert.AreEqual(results.GetValue(), 6);
+        }
 
-            // Test Zeros, Ones
+        [TestMethod]
+        public void BasicConstantZerosAndOnes()
+        {
+            // Test Zeros, Ones for n x n shape
             var o = g.Ones(new TFShape(4, 4));
-            var r = g.RandomNormal(new TFShape(4, 4));
-            var z = g.Zeros(new TFShape(4, 4));
-            var m = g.Mul(o, r);
-            var res1 = s.GetRunner().Run(m);
-            var res2 = s.GetRunner().Run(g.Mul(g.Mul(o, r), z));
+            Assert.IsNotNull(o);
+            Assert.AreEqual(o.OutputType, TFDataType.Double);
 
-            //Test Constants
+            var z = g.Zeros(new TFShape(4, 4));
+            Assert.IsNotNull(z);
+            Assert.AreEqual(z.OutputType, TFDataType.Double);
+
+            var r = g.RandomNormal(new TFShape(4, 4));
+            Assert.IsNotNull(r);
+            Assert.AreEqual(r.OutputType, TFDataType.Double);
+
+            var res1 = s.GetRunner().Run(g.Mul(o, r));
+            Assert.IsNotNull(res1);
+            Assert.AreEqual(res1.TensorType, TFDataType.Double);
+            Assert.AreEqual(res1.NumDims, 2);
+            Assert.AreEqual(res1.Shape[0], 4);
+            Assert.AreEqual(res1.Shape[1], 4);
+            Assert.AreEqual(res1.ToString(), "[4x4]");
+
+            var matval1 = res1.GetValue();
+            Assert.IsNotNull(matval1);
+            Assert.IsInstanceOfType(matval1, typeof(double[,]));
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Assert.IsNotNull(((double[,])matval1)[i, j]);
+                }
+            }
+
+            var res2 = s.GetRunner().Run(g.Mul(g.Mul(o, r), z));
+            Assert.IsNotNull(res2);
+            Assert.AreEqual(res2.TensorType, TFDataType.Double);
+            Assert.AreEqual(res2.NumDims, 2);
+            Assert.AreEqual(res2.Shape[0], 4);
+            Assert.AreEqual(res2.Shape[1], 4);
+            Assert.AreEqual(res2.ToString(), "[4x4]");
+
+            var matval2 = res2.GetValue();
+            Assert.IsNotNull(matval2);
+            Assert.IsInstanceOfType(matval2, typeof(double[,]));
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Assert.IsNotNull(((double[,])matval2)[i, j]);
+                    Assert.AreEqual(((double[,])matval2)[i, j], 0.0);
+                }
+            }
+
+        }
+
+        [TestMethod]
+        public void BasicConstantsOnSymmetricalShapes()
+        {
+            //build some test vectors
+            var o = g.Ones(new TFShape(4, 4));
+            var z = g.Zeros(new TFShape(4, 4));
+            var r = g.RandomNormal(new TFShape(4, 4));
+            var matval = s.GetRunner().Run(g.Mul(o, r)).GetValue();
+            var matvalzero = s.GetRunner().Run(g.Mul(g.Mul(o, r), z)).GetValue();
+
             var co = g.Constant(1.0, new TFShape(4, 4), TFDataType.Double);
             var cz = g.Constant(0.0, new TFShape(4, 4), TFDataType.Double);
-            var cr = g.RandomNormal(new TFShape(4, 4));
-            var cm = g.Mul(co, cr);
-            var cres1 = s.GetRunner().Run(cm);
-            var cres2 = s.GetRunner().Run(g.Mul(g.Mul(co, cr), cz));
+            var res1 = s.GetRunner().Run(g.Mul(co, r));
 
-            var so = g.Ones(new TFShape(4, 3));
-            var sr = g.RandomNormal(new TFShape(3, 5));
-            var sz = g.Zeros(new TFShape(5, 6));
-            var sm = g.MatMul(so, sr);
-            var sres1 = s.GetRunner().Run(sm);
-            var sres2 = s.GetRunner().Run(g.MatMul(g.MatMul(so, sr), sz));
+            Assert.IsNotNull(res1);
+            Assert.AreEqual(res1.TensorType, TFDataType.Double);
+            Assert.AreEqual(res1.NumDims, 2);
+            Assert.AreEqual(res1.Shape[0], 4);
+            Assert.AreEqual(res1.Shape[1], 4);
+            Assert.AreEqual(res1.ToString(), "[4x4]");
 
-            // TODO: API-wise, perhaps session.Run () can have a simple
-            // overload where we only care about the fetched values, 
-            // making the above:
-            // s.Run (g.Mul (a, b));
+            var cmatval1 = res1.GetValue();
+            Assert.IsNotNull(cmatval1);
+            Assert.IsInstanceOfType(cmatval1, typeof(double[,]));
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Assert.IsNotNull(((double[,])cmatval1)[i, j]);                    
+                }
+            }
+
+            var cres2 = s.GetRunner().Run(g.Mul(g.Mul(co, r), cz));
+
+            Assert.IsNotNull(cres2);
+            Assert.AreEqual(cres2.TensorType, TFDataType.Double);
+            Assert.AreEqual(cres2.NumDims, 2);
+            Assert.AreEqual(cres2.Shape[0], 4);
+            Assert.AreEqual(cres2.Shape[1], 4);
+            Assert.AreEqual(cres2.ToString(), "[4x4]");
+
+            var cmatval2 = cres2.GetValue();
+            Assert.IsNotNull(cmatval2);
+            Assert.IsInstanceOfType(cmatval2, typeof(double[,]));
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Assert.IsNotNull(((double[,])cmatval2)[i, j]);
+                    Assert.AreEqual(((double[,])matvalzero)[i, j], ((double[,])cmatval2)[i, j]);
+                }
+            }
+
+        }
+
+        [TestMethod]
+        public void BasicConstantsUnSymmetrical()
+        {
+            var o = g.Ones(new TFShape(4, 3));
+            Assert.IsNotNull(o);
+            Assert.AreEqual(o.OutputType, TFDataType.Double);
+
+            var r = g.RandomNormal(new TFShape(3, 5));
+            Assert.IsNotNull(o);
+            Assert.AreEqual(o.OutputType, TFDataType.Double);
+            
+            //expect incompatible shapes
+            Assert.ThrowsException<TFException>(() => s.GetRunner().Run(g.Mul(o, r)));
+            
+            var res = s.GetRunner().Run(g.MatMul(o, r));
+            Assert.IsNotNull(res);
+            Assert.AreEqual(res.TensorType, TFDataType.Double);
+            Assert.AreEqual(res.NumDims, 2);
+            Assert.AreEqual(res.Shape[0], 4);
+            Assert.AreEqual(res.Shape[1], 5);
+
+            double[,] val = (double[,])res.GetValue();
+            Assert.IsNotNull(val);
+            Assert.IsInstanceOfType(val, typeof(double[,]));
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Assert.IsNotNull(((double[,])val)[i, j]);
+                }
+            }
+            
+        }
+
+        [TestMethod]
+        public void BasicMultidimensionalArray()
+        {
+            var var_a = g.Placeholder(TFDataType.Int32);
+            var mul = g.Mul(var_a, g.Const(2));
+
+            var a = new int[,,] { { { 0, 1 }, { 2, 3 } }, { { 4, 5 }, { 6, 7 } } };
+            var res = s.GetRunner().AddInput(var_a, a).Fetch(mul).Run()[0];
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(res.TensorType, TFDataType.Int32);
+            Assert.AreEqual(res.NumDims, 3);
+            Assert.AreEqual(res.Shape[0], 2);
+            Assert.AreEqual(res.Shape[1], 2);
+            Assert.AreEqual(res.Shape[2], 2);
+
+            var actual = (int[,,])res.GetValue();
+            var expected = new int[,,] { { { 0, 2 }, { 4, 6 } }, { { 8, 10 }, { 12, 14 } } };
+
+            Assert.IsTrue(expected.Cast<int>().SequenceEqual(actual.Cast<int>()));
+        }
+
+        [TestMethod]
+        public void BasicMatrix()
+        {
+            // 1x2 matrix
+            var matrix1 = g.Const(new double[,] { { 3, 3 } });
+            // 2x1 matrix
+            var matrix2 = g.Const(new double[,] { { 2 }, { 2 } });
+
+            var expected = new double[,] { { 12 } };
+
+            var res = s.GetRunner().Run(g.MatMul(matrix1, matrix2));
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(res.TensorType, TFDataType.Double);
+            Assert.AreEqual(res.NumDims, 2);
+            Assert.AreEqual(res.Shape[0], 1);
+            Assert.AreEqual(res.Shape[1], 1);
+
+            double[,] val = (double[,])res.GetValue();
+            Assert.IsNotNull(val);
+            Assert.IsInstanceOfType(val, typeof(double[,]));
+
+            Assert.AreEqual(val[0, 0], expected[0,0]);
 
         }
 
