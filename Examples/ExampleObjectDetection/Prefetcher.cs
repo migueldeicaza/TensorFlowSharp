3860dd5885491c7f663a9ca11da66b8523b7b0cb
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2017-08-09 22:21:16 +0300

    added PaddingFIFOQueue

---
+++ Examples/ExampleObjectDetection/Prefetcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TensorFlow;
+
+namespace ExampleObjectDetection
+{
+    public static class Prefetcher
+    {
+        public static void Prefetch(TFGraph graph, Dictionary<string, string> tensorDictionary, int capacity)
+        {
+            //var names = tensorDictionary.Keys;
+            //TFDataType[] dtypes = tensorDictionary.Values.Select(x => x.dtype).ToArray();
+            //TFShape[] shapes = tensorDictionary.Values.Select(x => x.GetShape()).ToArray();
+
+            //var prefetchQueue = graph.PaddingFIFOQueueV2(dtypes, shapes, capacity, operName: "prefetch_queue");
+
+            //graph.Sca
+        }
+    }
+}
---
+++ TensorFlowSharp/Optimizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace TensorFlowSharp
+{
+    public class Optimizer
+    {
+        private bool _useLocking;
+        private string _name;
+        private Dictionary<string, string> _slots;
+
+        public Optimizer(bool useLocking, string name)
+        {
+            _useLocking = useLocking;
+            _name = name;
+            _slots = new Dictionary<string, string>();
+        }
+
+        public void Minimize()
+        {
+
+        }
+    }
+
+    public enum GateGradientsValues
+    {
+        None = 0,
+        Op = 1,
+        Graph = 2
+    }
+}
