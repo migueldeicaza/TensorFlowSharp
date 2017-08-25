9c8d7aaefe54a8b76d064b3b366b1bdcd037de95
César Souza <cesarsouza@gmail.com>
2017-08-25 06:56:30 +0200

    Adding clipping operations (#117)
    * Adding ClipByValue, ClipByNorm, GlobalNorm, ClipByAverageNorm, and Range.
    
    * Adding unit tests for the operations that have been just added.
    
    * Reformatting to follow the same style as the rest of the project.

---
+++ tests/TensorFlowSharp.Tests.CSharp/ArrayTests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using TensorFlow;
+using Xunit;
+
+namespace TensorFlowSharp.Tests.CSharp
+{
+    public class ArrayTests
+    {
+
+        private static IEnumerable<object[]> stackData()
+        {
+            // Example from https://www.tensorflow.org/api_docs/python/tf/stack
+
+            // 'x' is [1, 4]
+            // 'y' is [2, 5]
+            // 'z' is [3, 6]
+
+            double[] x = { 1, 4 };
+            double[] y = { 2, 5 };
+            double[] z = { 3, 6 };
+
+            // stack([x, y, z]) => [[1, 4], [2, 5], [3, 6]]  // Pack along first dim.
+            // stack([x, y, z], axis= 1) => [[1, 2, 3], [4, 5, 6]]
+
+            yield return new object[] { x, y, z, null,  new double[,] { { 1, 4 },
+                                                                        { 2, 5 },
+                                                                        { 3, 6 } } };
+
+            yield return new object[] { x, y, z, 1, new double[,] { { 1, 2, 3 },
+                                                                    { 4, 5, 6 } } };
+        }
+
+        [Theory]
+        [MemberData(nameof(stackData))]
+        public void Should_Stack(double[] x, double[] y, double[] z, int? axis, double[,] expected)
+        {
+            using (var graph = new TFGraph())
+            using (var session = new TFSession(graph))
+            {
+                var a = graph.Placeholder(TFDataType.Double, new TFShape(2));
+                var b = graph.Placeholder(TFDataType.Double, new TFShape(2));
+                var c = graph.Placeholder(TFDataType.Double, new TFShape(2));
+
+                TFOutput r = graph.Stack(new[] { a, b, c }, axis: axis);
+
+                TFTensor[] result = session.Run(new[] { a, b, c }, new TFTensor[] { x, y, z }, new[] { r });
+
+                double[,] actual = (double[,])result[0].GetValue();
+                TestUtils.MatrixEqual(expected, actual, precision: 10);
+            }
+        }
+
+
+
+
+
+        private static IEnumerable<object[]> rangeData()
+        {
+            double[] x = { 1, 4 };
+            double[] y = { 2, 5 };
+            double[] z = { 3, 6 };
+
+            // 'start' is 3
+            // 'limit' is 18
+            // 'delta' is 3
+            //  tf.range(start, limit, delta) ==> [3, 6, 9, 12, 15]
+
+            // 'start' is 3
+            // 'limit' is 1
+            // 'delta' is -0.5
+            //  tf.range(start, limit, delta) ==> [3, 2.5, 2, 1.5]
+
+            // 'limit' is 5
+            //  tf.range(limit) ==> [0, 1, 2, 3, 4]
+
+            yield return new object[] { 3, 18, 3, new int[] { 3, 6, 9, 12, 15 } };
+            yield return new object[] { 3, 1, -0.5, new double[] { 3, 2.5, 2, 1.5 } };
+            yield return new object[] { 3, 1, -0.5f, new float[] { 3, 2.5f, 2, 1.5f } };
+            yield return new object[] { null, 5, null, new int[] { 0, 1, 2, 3, 4 } };
+            yield return new object[] { null, 5f, null, new float[] { 0, 1, 2, 3, 4f } };
+        }
+
+        [Theory]
+        [MemberData(nameof(rangeData))]
+        public void Should_Range(object start, object limit, object delta, object expected)
+        {
+            // Examples from https://www.tensorflow.org/api_docs/python/tf/range
+
+            using (var graph = new TFGraph())
+            using (var session = new TFSession(graph))
+            {
+                TFOutput tstart = graph.Placeholder(start == null ? TFDataType.Int32 : TensorTypeFromType(start.GetType()));
+                TFOutput tlimit = graph.Placeholder(limit == null ? TFDataType.Int32 : TensorTypeFromType(limit.GetType()));
+                TFOutput tdelta = graph.Placeholder(delta == null ? TFDataType.Int32 : TensorTypeFromType(delta.GetType()));
+
+                TFTensor[] result;
+                if (start == null && delta == null)
+                {
+                    TFOutput r = graph.Range(tlimit);
+                    result = session.Run(new[] { tlimit }, new TFTensor[] { TensorFromObject(limit) }, new[] { r });
+                }
+                else
+                {
+                    TFOutput r = graph.Range(tstart, (Nullable<TFOutput>)tlimit, (Nullable<TFOutput>)tdelta);
+                    result = session.Run(new[] { tstart, tlimit, tdelta },
+                        new TFTensor[] { TensorFromObject(start), TensorFromObject(limit), TensorFromObject(delta) },
+                        new[] { r });
+                }
+
+                Array actual = (Array)result[0].GetValue();
+                TestUtils.MatrixEqual((Array)expected, actual, precision: 10);
+            }
+        }
+
+        public static TFDataType TensorTypeFromType(Type type)
+        {
+            if (type == typeof(float))
+                return TFDataType.Float;
+            if (type == typeof(double))
+                return TFDataType.Double;
+            if (type == typeof(int))
+                return TFDataType.Int32;
+            if (type == typeof(byte))
+                return TFDataType.UInt8;
+            if (type == typeof(short))
+                return TFDataType.Int16;
+            if (type == typeof(sbyte))
+                return TFDataType.Int8;
+            if (type == typeof(String))
+                return TFDataType.String;
+            if (type == typeof(bool))
+                return TFDataType.Bool;
+            if (type == typeof(long))
+                return TFDataType.Int64;
+            if (type == typeof(ushort))
+                return TFDataType.UInt16;
+            if (type == typeof(Complex))
+                return TFDataType.Complex128;
+
+            throw new ArgumentOutOfRangeException("type");
+        }
+
+        public static TFTensor TensorFromObject(object obj)
+        {
+            Type type = obj.GetType();
+            if (type == typeof(float))
+                return new TFTensor((float)obj);
+            if (type == typeof(double))
+                return new TFTensor((double)obj);
+            if (type == typeof(int))
+                return new TFTensor((int)obj);
+            if (type == typeof(byte))
+                return new TFTensor((byte)obj);
+            if (type == typeof(short))
+                return new TFTensor((short)obj);
+            if (type == typeof(sbyte))
+                return new TFTensor((sbyte)obj);
+            if (type == typeof(String))
+                throw new NotImplementedException();
+            if (type == typeof(bool))
+                return new TFTensor((bool)obj);
+            if (type == typeof(long))
+                return new TFTensor((long)obj);
+            if (type == typeof(ushort))
+                return new TFTensor((ushort)obj);
+            if (type == typeof(Complex))
+                return new TFTensor((Complex)obj);
+
+            throw new ArgumentOutOfRangeException("type");
+        }
+    }
+}
---
+++ tests/TensorFlowSharp.Tests.CSharp/ClipTests.cs
@@ -0,0 +1,269 @@
+﻿using System.Collections.Generic;
+using TensorFlow;
+using Xunit;
+
+namespace TensorFlowSharp.Tests.CSharp
+{
+    public class ClipTests
+    {
+        private static IEnumerable<object[]> clipByValueData()
+        {
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { 1, 2, 0 },
+                    { 4, -1, 7 }
+                },
+
+                1.0, 6.0,
+
+                new double[,]
+                {
+                    { 1, 2, 1 },
+                    { 4, 1, 6 }
+                }
+            };
+
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { -9, double.PositiveInfinity, double.NegativeInfinity },
+                    { 4, double.PositiveInfinity, 7 }
+                },
+
+                -2, 42,
+
+                new double[,]
+                {
+                    { -2, 42, -2 },
+                    { 4, 42, 7 }
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(clipByValueData))]
+        public void Should_ClipByValue(double[,] m, double min, double max, double[,] expected)
+        {
+            using (var graph = new TFGraph())
+            using (var session = new TFSession(graph))
+            {
+                var matrix = graph.Placeholder(TFDataType.Double);
+                var clip_min = graph.Placeholder(TFDataType.Double);
+                var clip_max = graph.Const(new TFTensor(max));
+
+                TFOutput y = graph.ClipByValue(matrix, clip_min, clip_max);
+
+                TFTensor[] result = session.Run(new[] { matrix, clip_min }, new TFTensor[] { m, min }, new[] { y });
+
+                double[,] actual = (double[,])result[0].GetValue();
+                Assert.Equal(expected, actual);
+            }
+        }
+
+
+
+
+
+
+        private static IEnumerable<object[]> clipByNormData()
+        {
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { 1, 2, 0 },
+                    { 4, -1, 7 }
+                },
+
+                1.0, 0,
+
+                new double[,]
+                {
+                    { 0.24253562503633297, 0.89442719099991586, 0 },
+                    { 0.97014250014533188, -0.44721359549995793, 1 }
+                }
+            };
+
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { -9, 100, 0.1 },
+                    { 4, 0.4, 7 }
+                },
+
+                -2, 1,
+
+                new double[,]
+                {
+                    { -9, 100, 0.1 },
+                    { 4, 0.4, 7 }
+                }
+            };
+
+            yield return new object[]
+{
+                new double[,]
+                {
+                    { 1e-10, 1e-5, 1e-3 },
+                    { 1e-2, 0.0, 7.0 }
+                },
+
+                -2, 2,
+
+                null
+            };
+
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { -9, double.PositiveInfinity, double.NegativeInfinity },
+                    { 4, double.PositiveInfinity, 7 }
+                },
+
+                -2, -1,
+
+                new double[,]
+                {
+                    { -9, double.PositiveInfinity, double.NegativeInfinity },
+                    { 4, double.PositiveInfinity, 7 }
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(clipByNormData))]
+        public void Should_ClipByNorm(double[,] m, double norm, int axis, double[,] expected)
+        {
+            using (var graph = new TFGraph())
+            using (var session = new TFSession(graph))
+            {
+                var matrix = graph.Placeholder(TFDataType.Double);
+                var clip_norm = graph.Placeholder(TFDataType.Double);
+                var a = graph.Const(new TFTensor(axis));
+
+                TFOutput y = graph.ClipByNorm(matrix, clip_norm, a);
+
+                if (expected != null)
+                {
+                    TFTensor[] result = session.Run(new[] { matrix, clip_norm }, new TFTensor[] { m, norm }, new[] { y });
+
+                    double[,] actual = (double[,])result[0].GetValue();
+                    TestUtils.MatrixEqual(expected, actual, precision: 10);
+                }
+                else
+                {
+                    Assert.Throws<TFException>(() => session.Run(new[] { matrix, clip_norm }, new TFTensor[] { m, norm }, new[] { y }));
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        private static IEnumerable<object[]> clipByAverageNormData()
+        {
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { 1, 2, 0 },
+                    { 4, -1, 7 }
+                },
+
+                1.0, 0,
+
+                new double[,]
+                {
+                    { 0.24253562503633297, 0.89442719099991586, 0 },
+                    { 0.97014250014533188, -0.44721359549995793, 1 }
+                }
+            };
+
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { -9, 100, 0.1 },
+                    { 4, 0.4, 7 }
+                },
+
+                -2, 1,
+
+                new double[,]
+                {
+                    { -9, 100, 0.1 },
+                    { 4, 0.4, 7 }
+                }
+            };
+
+            yield return new object[]
+{
+                new double[,]
+                {
+                    { 1e-10, 1e-5, 1e-3 },
+                    { 1e-2, 0.0, 7.0 }
+                },
+
+                -2, 2,
+
+                null
+            };
+
+            yield return new object[]
+            {
+                new double[,]
+                {
+                    { -9, double.PositiveInfinity, double.NegativeInfinity },
+                    { 4, double.PositiveInfinity, 7 }
+                },
+
+                -2, -1,
+
+                new double[,]
+                {
+                    { -9, double.PositiveInfinity, double.NegativeInfinity },
+                    { 4, double.PositiveInfinity, 7 }
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(clipByAverageNormData))]
+        public void Should_ClipByAverageNorm(double[,] m, double norm, int axis, double[,] expected)
+        {
+            using (var graph = new TFGraph())
+            using (var session = new TFSession(graph))
+            {
+                var matrix = graph.Placeholder(TFDataType.Double);
+                var clip_norm = graph.Placeholder(TFDataType.Double);
+                var a = graph.Const(new TFTensor(axis));
+
+                TFOutput y = graph.ClipByNorm(matrix, clip_norm, a);
+
+                if (expected != null)
+                {
+                    TFTensor[] result = session.Run(new[] { matrix, clip_norm }, new TFTensor[] { m, norm }, new[] { y });
+
+                    double[,] actual = (double[,])result[0].GetValue();
+                    TestUtils.MatrixEqual(expected, actual, precision: 10);
+                }
+                else
+                {
+                    Assert.Throws<TFException>(() => session.Run(new[] { matrix, clip_norm }, new TFTensor[] { m, norm }, new[] { y }));
+                }
+            }
+        }
+
+    }
+}
