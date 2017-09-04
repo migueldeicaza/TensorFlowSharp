10ed7680022ecee1c9e4ea9357c497d564d4e597
Dorokhov <dorokhov.vladimir@gmail.com>
2017-09-04 06:07:42 +0300

    Test for TanhGrad

--- tests/TensorFlowSharp.Tests.CSharp/MathTests.cs
+++ tests/TensorFlowSharp.Tests.CSharp/MathTests.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using TensorFlow;
 using Xunit;
 
@@ -6,6 +10,25 @@ namespace TensorFlowSharp.Tests.CSharp
 {
 	public class MathTests
 	{
+		[Fact]
+		public void Should_CalculateTanhGrad_Correctly ()
+		{
+			using (TFGraph graph = new TFGraph ())
+			using (TFSession session = new TFSession (graph)) 
+			{
+
+				TFOutput x = graph.Const (new TFTensor (0.7));
+				TFOutput y = graph.Tanh (x);
+				TFOutput dy = graph.Const (new TFTensor (new [] { 1.0 }));
+				TFOutput grad = graph.TanhGrad (y, dy);
+
+				TFTensor [] result = session.Run (new TFOutput [] { }, new TFTensor [] { }, new [] { grad });
+
+				double value = (double)result [0].GetValue ();
+				Assert.Equal (0.634739589982459, value, 15);
+			}
+		}
+
 		private static IEnumerable<object []> reduceMeanData ()
 		{
 			// Example from https://www.tensorflow.org/api_docs/python/tf/reduce_mean
@@ -48,6 +71,5 @@ namespace TensorFlowSharp.Tests.CSharp
 				}
 			}
 		}
-
 	}
 }
