9e6602e5d657039709aaeab2ceaf666c5283db80
Dor <dorokhov.vladimir@gmail.com>
2017-09-08 11:09:17 +0300

    CI tests

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR/packages/xunit.runner.console.2.2.0/tools
-mono --arch=64 xunit.console.exe "$TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests/bin/Debug/TensorFlowSharp.Tests.dll" "$TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests.CSharp/bin/Debug/TensorFlowSharp.Tests.CSharp.dll"
+mono --arch=64 xunit.console.exe "$TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests/bin/Release/TensorFlowSharp.Tests.dll" "$TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests.CSharp/bin/Release/TensorFlowSharp.Tests.CSharp.dll"
