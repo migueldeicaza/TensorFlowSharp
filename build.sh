eff0287c18fdd95cfae3d8a2454e5a63541cf834
Dor <dorokhov.vladimir@gmail.com>
2017-09-08 08:43:44 +0300

    CI tests

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd $TRAVIS_BUILD_DIR/packages/xunit.runner.console.2.2.0/tools
-mono --arch=64 xunit.console.exe $TRAVIS_BUILD_DIR/tests/TensorFlowSharp.Tests/bin/Release/TensorFlowSharp.Tests.dll
