6194711eb12defb7f6cce40d471a3f49f83f411c
Miguel de Icaza <miguel@gnome.org>
2017-09-14 21:10:10 -0400

    Fix sample

--- SampleTest/SampleTest.cs
+++ SampleTest/SampleTest.cs
@@ -291,9 +291,10 @@ namespace SampleTest
 				var X = g.Placeholder (TFDataType.Float);
 				var Y = g.Placeholder (TFDataType.Float);
 
-				var W = g.Variable (g.Const ((float)rng.Next ()), operName: "weight");
-				var b = g.Variable (g.Const ((float) rng.Next ()), operName: "bias");
-				var pred = g.Add (g.Mul (X, W, "x*w"), b);
+				TFOutput readW, readB;
+				var W = g.Variable (g.Const ((float)rng.Next ()), out readW, operName: "weight");
+				var b = g.Variable (g.Const ((float) rng.Next ()), out readB, operName: "bias");
+				var pred = g.Add (g.Mul (X, readW, "x*w"), readB);
 
 				var first = g.Pow (g.Sub (pred, Y), g.Const ((float)2));
 				var cost = g.Div (g.ReduceSum (g.Pow (g.Sub (pred, Y), g.Const (2f))), g.Mul (g.Const (2f), g.Const ((float)n_samples), "2*n_samples"));
