597012ad8d59459169fd5d2eba59e799f5e875f5
Dorokhov <dorokhov.vladimir@gmail.com>
2017-10-11 21:08:51 +0300

    bug fix test coverage

--- tests/TensorFlowSharp.Tests.CSharp/GradientTests.cs
+++ tests/TensorFlowSharp.Tests.CSharp/GradientTests.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
-using TensorFlow;
+﻿using TensorFlow;
 using Xunit;
 
 namespace TensorFlowSharp.Tests.CSharp
 {
-    public class GradientTests
-    {
+	public class GradientTests
+	{
 		[Fact]
-		public void Should_Tests ()
+		public void ShouldAddGradients ()
 		{
 			using (var graph = new TFGraph ())
 			using (var session = new TFSession (graph)) {
@@ -20,13 +16,12 @@ namespace TensorFlowSharp.Tests.CSharp
 				var y2 = graph.Square (y1, "Square2");
 
 				var y3 = graph.Square (y2, "Square3");
-				var g = graph.AddGradients (new TFOutput [] {y1,y2 }, new [] { x },new TFOutput [] { graph.Const(3) });
+				var g = graph.AddGradients (new TFOutput [] { y1, y3 }, new [] { x });
 
 				var r = session.Run (new TFOutput [] { }, new TFTensor [] { }, g);
 				int dy = (int)r [0].GetValue ();
-				int dy2 = (int)r [1].GetValue ();
-				Assert.Equal (17502.0, dy + dy2);
+				Assert.Equal (17502.0, dy);
 			}
 		}
-    }
+	}
 }
