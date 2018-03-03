268e84b7a8f65aeaa87e33ead99d299422c10e8a
miguel <miguel.de.icaza@gmail.com>
2018-03-02 23:50:52 -0500

    Add convenience SaveTensors, RestoreTensor methods to TFSession

--- TensorFlowSharp/Tensorflow.cs
+++ TensorFlowSharp/Tensorflow.cs
@@ -2853,6 +2853,65 @@ namespace TensorFlow
 			}
 			return result;
 		}
+
+		/// <summary>
+		/// Restores a tensor from a serialized tensorflor file.
+		/// </summary>
+		/// <returns>The deserialized tensor from the file.</returns>
+		/// <param name="filename">File containing your saved tensors.</param>
+		/// <param name="tensor">The name that was used to save the tensor.</param>
+		/// <param name="type">The data type for the tensor.</param>
+		/// <code>
+		/// using (var session = new TFSession()){
+		///   var a = session.Graph.Const(30, "a");
+		///   var b = session.Graph.Const(12, "b");
+		///   var multiplyResults = session.GetRunner().Run(session.Graph.Add(a, b));
+		///   var multiplyResultValue = multiplyResults.GetValue();
+		///   Console.WriteLine("a*b={0}", multiplyResultValue);
+		///   session.SaveTensors($"saved.tsf", ("a", a), ("b", b));
+		/// }
+		/// </code>
+		public TFOutput RestoreTensor (string filename, string tensor, TFDataType type)
+		{
+			return this.Graph.Restore (this.Graph.Const (TFTensor.CreateString (Encoding.UTF8.GetBytes (filename))),
+							   this.Graph.Const (TFTensor.CreateString (Encoding.UTF8.GetBytes (tensor))),
+						   type);
+		}
+
+		/// <summary>
+		/// Saves the tensors in the session to a file.
+		/// </summary>
+		/// <returns>The tensors.</returns>
+		/// <param name="filename">File to store the tensors in (for example: tensors.tsf).</param>
+		/// <param name="tensors">An array of tuples that include the name you want to give the tensor on output, and the tensor you want to save.</param>
+		/// <remarks>
+		/// <para>
+		/// Tensors saved with this method can be loaded by calling <see cref="M:RestoreTensor"/>.
+		/// </para>
+		/// <code>
+		/// using (var session = new TFSession ()) {
+		///   var a = session.Graph.Const(30, "a");
+		///   var b = session.Graph.Const(12, "b");
+		///   var multiplyResults = session.GetRunner().Run(session.Graph.Add(a, b));
+		///   var multiplyResultValue = multiplyResults.GetValue();
+		///   Console.WriteLine("a*b={0}", multiplyResultValue);
+		///   session.SaveTensors($"saved.tsf", ("a", a), ("b", b));
+		/// }
+		/// </code>
+		/// </remarks>
+		public TFTensor [] SaveTensors (string filename, params ValueTuple<string, TFOutput> [] tensors)
+		{
+			return GetRunner ().AddTarget (Graph.Save (Graph.Const (TFTensor.CreateString (Encoding.UTF8.GetBytes (filename)), TFDataType.String),
+				      Graph.Concat (Graph.Const (0), tensors.Select (T => {
+					      TFTensor clone = TFTensor.CreateString (Encoding.UTF8.GetBytes (T.Item1));
+					      return Graph.Const (new TFTensor (TFDataType.String,
+											  new long [] { 1 },
+											  clone.Data,
+											  clone.TensorByteSize,
+											  null, IntPtr.Zero));
+				      }).ToArray ()), tensors.Select (d => d.Item2).ToArray ())).Run ();
+		}
+   
 	}
 
 	/// <summary>
