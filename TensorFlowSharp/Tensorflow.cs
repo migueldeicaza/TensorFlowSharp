e0b7c85f554c0fe1c6d11f83f5ab69c24da8f3c9
alexpantyukhin <apantykhin@gmail.com>
2018-03-21 11:19:48 +0400

    free device list.

--- TensorFlowSharp/Tensorflow.cs
+++ TensorFlowSharp/Tensorflow.cs
@@ -2488,8 +2488,7 @@ namespace TensorFlow
 				list.Add (new DeviceAttributes (name, deviceType, memory));
 			}
 
-			// TODO: Fix deleting.
-			// TF_DeleteDeviceList (rawDeviceList);
+			TF_DeleteDeviceList (rawDeviceList);
 
 			return list;
 		}
