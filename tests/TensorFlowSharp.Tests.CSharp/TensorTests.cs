7b126fb1d4c8c5e5428e4d5f44e4fcd8b9a17772
Zeeshan <zeeshan.ahmed.za@gmail.com>
2019-02-15 20:19:35 +0000

    Added methods to support string input and output from TensorFlow.

--- tests/TensorFlowSharp.Tests.CSharp/TensorTests.cs
+++ tests/TensorFlowSharp.Tests.CSharp/TensorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using TensorFlow;
+using System.Text;
 using Xunit;
 
 namespace TensorFlowSharp.Tests.CSharp
@@ -60,5 +61,56 @@ namespace TensorFlowSharp.Tests.CSharp
 			}
 		}
 
-	}
+        [Fact]
+        public void StringTestWithMultiDimStringTensorAsInputOutput()
+        {
+            using (var graph = new TFGraph())
+            using (var session = new TFSession(graph))
+            {
+                var W = graph.Placeholder(TFDataType.String, new TFShape(-1, 2));
+                var identityW  = graph.Identity(W);
+
+                var dataW = new string[,] { { "This is fine.", "That's ok." }, { "This is fine.", "That's ok." } };
+                var bytes = new byte[2 * 2][];
+                bytes[0] = Encoding.UTF8.GetBytes(dataW[0, 0]);
+                bytes[1] = Encoding.UTF8.GetBytes(dataW[0, 1]);
+                bytes[2] = Encoding.UTF8.GetBytes(dataW[1, 0]);
+                bytes[3] = Encoding.UTF8.GetBytes(dataW[1, 1]);
+                var tensorW = TFTensor.CreateString(bytes, new TFShape(2,2));
+
+                var outputTensor = session.Run(new TFOutput[] { W }, new TFTensor[] { tensorW }, new[] { identityW });
+                
+                var outputW = TFTensor.DecodeMultiDimensionString(outputTensor[0]);
+                Assert.Equal(dataW[0, 0], Encoding.UTF8.GetString(outputW[0]));
+                Assert.Equal(dataW[0, 1], Encoding.UTF8.GetString(outputW[1]));
+                Assert.Equal(dataW[1, 0], Encoding.UTF8.GetString(outputW[2]));
+                Assert.Equal(dataW[1, 1], Encoding.UTF8.GetString(outputW[3]));
+            }
+        }
+
+        [Fact]
+        public void StringTestWithMultiDimStringTensorAsInputAndScalarStringAsOutput()
+        {
+            using (var graph = new TFGraph())
+            using (var session = new TFSession(graph))
+            {
+                var X = graph.Placeholder(TFDataType.String, new TFShape(-1));
+                var delimiter = graph.Const(TFTensor.CreateString(Encoding.UTF8.GetBytes("/")));
+                var indices = graph.Const(0);
+                var Y = graph.ReduceJoin(graph.StringSplit(X, delimiter).values, indices, separator: " ");
+
+                var dataX = new string[] { "Thank/you/very/much!.", "I/am/grateful/to/you.", "So/nice/of/you." };
+                var bytes = new byte[dataX.Length][];
+                bytes[0] = Encoding.UTF8.GetBytes(dataX[0]);
+                bytes[1] = Encoding.UTF8.GetBytes(dataX[1]);
+                bytes[2] = Encoding.UTF8.GetBytes(dataX[2]);
+                var tensorX = TFTensor.CreateString(bytes, new TFShape(3));
+
+                var outputTensors = session.Run(new TFOutput[] { X }, new TFTensor[] { tensorX }, new[] { Y });
+
+                var outputY = Encoding.UTF8.GetString(TFTensor.DecodeString(outputTensors[0]));
+                Assert.Equal(string.Join(" ", dataX).Replace("/", " "), outputY);
+            }
+        }
+    }
 }
