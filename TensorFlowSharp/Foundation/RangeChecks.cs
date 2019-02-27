c800d8a743ab4c8750bc07b8056d395f79069bbd
Erik Nyström <grevor.priest@gmail.com>
2019-02-27 19:16:50 +0100

    Added notion of lifetime to TFOperation, TFOutput and TFInput, dependent on their graph.

---
+++ TensorFlowSharp/Foundation/RangeChecks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace TensorFlowSharp.Foundation
+{
+    internal static class RangeChecks
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsWithinRange(int value, int minInclusive, int maxExclusive) => minInclusive <= value && value < maxExclusive;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsWithinRange<T>(T obj, T minInclusive, T maxExcluded)
+            where T : IComparable<T>
+        {
+            return minInclusive.CompareTo(obj) <= 0 && 0 < maxExcluded.CompareTo(obj);
+        }
+
+        public static int Assert(int value, string name, int minInclusive, int maxExclusive)
+        {
+            return IsWithinRange(value, minInclusive, maxExclusive)
+                ? value
+                : throw new ArgumentOutOfRangeException(FormattableString.Invariant($"Expected '{name}' to be within [{minInclusive}, {maxExclusive}), but was {value}"));
+        }
+    }
+}
---
+++ tests/TensorFlowSharp.Tests.CSharp/TFOperationTests.cs
@@ -0,0 +1,43 @@
+﻿using TensorFlow;
+using Xunit;
+
+namespace TensorFlowSharp.Tests.CSharp
+{
+
+    public class TFOperationTests
+    {
+        [Fact]
+        public void InputsEqualToTheIncommingNumberOfOutputsAreRetrievedInOrder()
+        {
+            using (var graph = new TFGraph())
+            {
+                var a = graph.Const(0f);
+                var v2 = graph.Variable(graph.Const(0.6f));
+
+                var add = graph.Add(a, v2.Read);
+
+                AssertExpectedNumberOfInputs(0, a.Operation);
+                AssertExpectedNumberOfInputs(1, v2.Read.Operation);
+                AssertExpectedNumberOfInputs(2, add.Operation);
+            }
+        }
+
+
+        private static void AssertExpectedNumberOfInputs(int expected, TFOperation sut)
+        {
+            Assert.Equal(expected, sut.NumInputs);
+
+            var inputs = sut.Inputs;
+
+            Assert.NotNull(inputs);
+            Assert.Equal(expected, inputs.Length);
+
+            for(int i = 0; i < inputs.Length; ++i)
+            {
+                var input = inputs[i];
+                Assert.Equal(i, input.Index);
+                Assert.Equal(sut.Handle, input.Operation);
+            }
+        }
+    }
+}
